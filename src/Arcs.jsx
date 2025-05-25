1e969ae405615ffb4d789f7f545e8e6ccb682cb7
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-25 20:28:36 +0200

    Fix parse bug

--- src/Arcs.jsx
+++ src/Arcs.jsx
@@ -7,7 +7,7 @@ import { Hub } from "nerdy-lib";
 
 const Arcs = () => {
 	// Checks if there is a game state in local storage and sets it to the gameState state variable. If not, it sets the gameState to the empty game state.
-	const [gameState, setGameState] = useState(localStorage.getItem(ARCS_STATE) ? JSON.parse(localStorage.getItem(ARCS_STATE)) : JSON.stringify(EMPTY_GAME_STATE))
+	const [gameState, setGameState] = useState(localStorage.getItem(ARCS_STATE) ? JSON.parse(localStorage.getItem(ARCS_STATE)) : JSON.parse(JSON.stringify(EMPTY_GAME_STATE)))
 
 	// Sets the game state in local storage and updates the gameState state variable.
 	const handleGameStateChange = (object) => {
